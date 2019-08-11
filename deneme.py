c94f71b8796c61666929bc461d107bd4fecae0f4
Berke Tuna <btuna14@ku.edu.tr>
2019-08-11 21:16:22 +0300

    Atom editor "push" trial

--- deneme.py
+++ deneme.py
@@ -1,14 +1,8 @@
-def anti_vowel(text):
-  text = str(text)
-  for i in range(len(text)):
-    if text[i] == "a" or text[i] == "A"or \
-       text[i] == "e" or text[i] == "E" or \
-       text[i] == "i" or text[i] == "I" or \
-       text[i] == "o" or text[i] == "O" or \
-       text[i] == "u" or text[i] == "U":
-      text[i] = text.replace(text[i] , "")
-  return text
-
-txt = "Hey You!"
-txt_new = anti_vowel(txt)
-print txt_new
+numbers = range(1,51)
+x = []
+i = 0
+for number in numbers:
+    product = number**2
+    x.append(product)
+print x
+print " a "
